2a41aeef99a803421091259425c7ffb1cfb4fba0
Stanley Pan <imoment.developer@gmail.com>
2022-02-04 15:51:31 -0500

    Added Extensions, working on OnBoardingPage UI layout

---
+++ Online_Shop2/Helpers/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Online_Shop2
+//
+//  Created by Stanley Pan on 2022/02/04.
+//
+
+import SwiftUI
+
+extension View {
+    
+    func getScreenSize() -> CGRect {
+        return UIScreen.main.bounds
+    }
+}
--- Online_Shop2/Views/OnBoardingPage.swift
+++ Online_Shop2/Views/OnBoardingPage.swift
@@ -11,10 +11,37 @@ let customFont = "Raleway-Regular"
 
 struct OnBoardingPage: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Find your\nGadget")
                 .font(.custom(customFont, size: 55))
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+            
+            Image("onboardBg")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Button {
+                
+            } label: {
+                Text("Get Started")
+                    .font(.custom(customFont, size: 18))
+                    .fontWeight(.semibold)
+                    .padding(.vertical, 18)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.1), radius: 5, x: 5, y: 5)
+                    .foregroundColor(Color("purple01"))
+            }
+            .padding(.horizontal, 30)
+            
+            .offset(y: getScreenSize().height < 750 ? 20 : 40)
+            
+            Spacer()
         }
+        .padding()
+        .padding(.top, getScreenSize().height < 750 ? 0 : 20)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(
             Color("purple01")
